0ddc2983ee6e07908141fc5fc3c851a0cab29b40
Sung Jong (David) Lee <davidsunglee2005@gmail.com>
2024-11-13 10:04:20 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div id="quote-box" className="App">
+      <div id="text"></div>
+      <div id="author"></div>
+      <button id="new-quote">New Quote</button>
+      <a href="#">Tweet Quote</a>
+    </div>
+  );
+}
+
+export default App;
